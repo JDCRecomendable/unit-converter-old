6a37f46207f85767a9acaebca12ae3fb9ec2d59b
JDCRecomendable <JDCRecomendable@protonmail.ch>
2019-05-15 00:30:39 +1200

    Remove unnecessary code

--- Unit Converter/Resources/StatusMessages.cs
+++ Unit Converter/Resources/StatusMessages.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Unit_Converter
+﻿namespace Unit_Converter
 {
     public static class StatusMessages
     {
